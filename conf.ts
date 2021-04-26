ce346da942d0048ab1ca73440d5eb1f4a808c93c
张扬 <15171255945@163.com>
2021-04-26 15:40:04 +0800

    chore: :hammer: 针对不同环境使用不同的配置

--- conf.ts
+++ conf.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: zhangyang
  * @Date: 2021-04-08 17:49:14
- * @LastEditTime: 2021-04-24 19:22:36
+ * @LastEditTime: 2021-04-26 15:38:26
  * @Description: 配置文件
  */
-import { resolve } from 'path';
+import { basename, resolve } from 'path';
 
 enum Structor {
   '签名过期' = '100-0-0',
@@ -13,25 +13,37 @@ enum Structor {
   '获取当前用户信息' = '200-0-0'
 }
 
-export default {
-  CONF_HTTP_PORT: 1443,
-  CONF_WS_PORT: 9527,
-  CONF_ORM: {
-    type: 'mysql',
-    host: '172.18.0.4',
-    port: 3306,
-    username: 'root',
-    password: 'my-secret-pw',
-    database: 'orm_chat_demo',
-    synchronize: true,
-    logging: false,
-    entities: [resolve(__dirname, 'src/entity/**/*{.ts,.js}')],
-    migrations: [resolve(__dirname, 'src/migration/**/*{.ts,.js}')],
-    subscribers: [resolve(__dirname, 'src/subscriber/**/*{.ts,.js}')]
- },
- CONF_REDIS: {
-  host: '172.18.0.5'
- },
- CONF_TOKEN_KEY: 'bluesyoung-web',
- Structor
-}
+const conf = (() => {
+  const env = process.env.APP_ENV;
+  // 部署配置 --- 树莓派
+  const base = {
+    CONF_HTTP_PORT: 1443,
+    CONF_WS_PORT: 9527,
+    CONF_ORM: {
+      type: 'mysql',
+      host: '172.18.0.4',
+      port: 3306,
+      username: 'root',
+      password: 'my-secret-pw',
+      database: 'orm_chat_demo',
+      synchronize: true,
+      logging: false,
+      entities: [resolve(__dirname, 'src/entity/**/*{.ts,.js}')],
+      migrations: [resolve(__dirname, 'src/migration/**/*{.ts,.js}')],
+      subscribers: [resolve(__dirname, 'src/subscriber/**/*{.ts,.js}')]
+    },
+    CONF_REDIS: {
+      host: '172.18.0.5'
+    },
+    CONF_TOKEN_KEY: 'bluesyoung-web',
+    Structor
+  };
+  if (env) {
+    // 开发配置 --- 本机
+    base.CONF_ORM.host = 'localhost';
+    base.CONF_ORM.password = 'root';
+    base.CONF_REDIS.host = 'localhost';
+  }
+  return base;
+})();
+export default conf;
