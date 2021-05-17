8b41ae1fec94e6ab3c2a2e4555f139e524e70d98
张扬 <15171255945@163.com>
2021-05-17 17:55:55 +0800

    feat: :sparkles: 实现 websocket 上传文件的demo

--- src/model/socket.ts
+++ src/model/socket.ts
@@ -1,10 +1,12 @@
 /*
  * @Author: zhangyang
  * @Date: 2021-04-09 14:10:41
- * @LastEditTime: 2021-04-14 09:48:46
+ * @LastEditTime: 2021-05-17 17:33:53
  * @Description: 管理 Websocket 消息
  */
 
+import { createWriteStream } from "fs";
+import { PassThrough } from "stream";
 import WebSocket from "ws";
 import { UserController } from './../controller/UserController';
 
@@ -15,6 +17,9 @@ export class MySocket {
   private socketPool: Map<string, WebSocket>;
 
   private user_controller: UserController;
+
+  private fileName: string;
+  private fileType: 'img' | 'audio';
   constructor(uid: string, conn: WebSocket, pool: Map<string, WebSocket>) {
     this.uid = uid;
     this.conn = conn;
@@ -25,8 +30,6 @@ export class MySocket {
 
   init() {
     this.user_controller = new UserController(this.uid, this.conn);
-
-
     this.user_controller.getUserInfo();
   }
 
@@ -36,9 +39,24 @@ export class MySocket {
   }
 
   msgProcess(str: any) {
-    console.log('消息处理');
-    console.log(str);
-    
+    console.log('---消息处理---');
+    if (Buffer.isBuffer(str)) {
+      const file = createWriteStream(__dirname + `../../../public/${this.fileType}/${this.fileName}`);
+      const buff = new PassThrough();
+      buff.end(str);
+      buff.pipe(file);
+    } else {
+      const { com, task, extra } = str;
+      if (com === 999) {
+        const { fileName } = extra;
+        this.fileName = fileName;
+        if (task === 1) {
+          this.fileType = 'img';
+        } else if (task === 2) {
+          this.fileType = 'audio';
+        }
+      }
+    }
   }
 
   offLineSend() {
